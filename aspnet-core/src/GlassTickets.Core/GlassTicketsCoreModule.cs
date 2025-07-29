33cc62724912e45a19cee93bb51d8a473b7d15c8
Benny <tarr184pro@gmail.com>
2025-07-29 17:12:25 +0200

    Added Time Converter For Postgres

--- aspnet-core/src/GlassTickets.Core/GlassTicketsCoreModule.cs
+++ aspnet-core/src/GlassTickets.Core/GlassTicketsCoreModule.cs
@@ -19,6 +19,7 @@ namespace GlassTickets
     {
         public override void PreInitialize()
         {
+            Clock.Provider = ClockProviders.Utc;
             Configuration.Auditing.IsEnabledForAnonymousUsers = true;
 
             // Declare entity types
