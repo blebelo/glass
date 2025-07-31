46c2e191541d3f534205777182f162ac4f9a68eb
Benny <tarr184pro@gmail.com>
2025-07-31 14:37:40 +0200

    Implemented Migrations For Entities

--- aspnet-core/src/GlassTickets.Core/Entities/Ticket.cs
+++ aspnet-core/src/GlassTickets.Core/Entities/Ticket.cs
@@ -4,22 +4,21 @@ using System.Collections.Generic;
 
 namespace GlassTickets.Entities
 {
-    public class Ticket: FullAuditedEntity<Guid>
+    public class Ticket : FullAuditedEntity<Guid>
     {
-            public string ReferenceNumber { get; set; }
-            public PriorityLevelEnum PriorityLevel { get; set; }
-            public string Location { get; set; }
-            public StatusEnum Status { get; set; }
-            public string Category { get; set; }
-            public string Description { get; set; }
-            public DateTime DateCreated { get; set; }
-            public DateTime LastUpdated { get; set; }
-            public DateTime? DateClosed { get; set; }
-            public string ReasonClosed { get; set; }
-            public bool SendUpdates { get; set; }
-            public string CustomerContactNumber { get; set; }
-            public virtual ICollection<Employee> AssignedEmployees { get; set; } = new List<Employee>();
+        public string ReferenceNumber { get; set; }
+        public PriorityLevelEnum PriorityLevel { get; set; }
+        public string Location { get; set; }
+        public StatusEnum Status { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public DateTime? DateClosed { get; set; }
+        public string ReasonClosed { get; set; }
+        public bool SendUpdates { get; set; }
+        public string CustomerContactNumber { get; set; }
+        public virtual ICollection<Employee> AssignedEmployees { get; set; } = new List<Employee>();
 
-        }
     }
 }
