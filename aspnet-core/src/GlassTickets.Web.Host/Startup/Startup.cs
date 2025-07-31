d5abe3c53b118f716820d716af0cf348e3280eb4
Benny <tarr184pro@gmail.com>
2025-07-31 06:22:29 +0200

    Disabled HTTPS

--- aspnet-core/src/GlassTickets.Web.Host/Startup/Startup.cs
+++ aspnet-core/src/GlassTickets.Web.Host/Startup/Startup.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Castle.Facilities.Logging;
-using Abp.AspNetCore;
+﻿using Abp.AspNetCore;
 using Abp.AspNetCore.Mvc.Antiforgery;
+using Abp.AspNetCore.SignalR.Hubs;
 using Abp.Castle.Logging.Log4Net;
 using Abp.Extensions;
+using Castle.Facilities.Logging;
 using GlassTickets.Configuration;
 using GlassTickets.Identity;
-using Abp.AspNetCore.SignalR.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace GlassTickets.Web.Host.Startup
 {
@@ -48,23 +50,24 @@ namespace GlassTickets.Web.Host.Startup
 
             services.AddSignalR();
 
-            // Configure CORS for angular2 UI
-            services.AddCors(
-                options => options.AddPolicy(
-                    _defaultCorsPolicyName,
-                    builder => builder
-                        .WithOrigins(
-                            // App:CorsOrigins in appsettings.json can contain more than one address separated by comma.
-                            _appConfiguration["App:CorsOrigins"]
-                                .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                .Select(o => o.RemovePostFix("/"))
-                                .ToArray()
-                        )
-                        .AllowAnyHeader()
+            //Disable HTTPS
+            services.Configure<HttpsRedirectionOptions>(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = null;
+            });
+
+            //CORS Service
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()
                         .AllowAnyMethod()
-                        .AllowCredentials()
-                )
-            );
+                        .AllowAnyHeader();
+                });
+            });
 
             // Swagger - Enable this line and the related lines in Configure method to enable swagger UI
             ConfigureSwagger(services);
